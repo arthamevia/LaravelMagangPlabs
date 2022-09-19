987e017a893325227d82b4127b6fcda95cdf6616
arthamevia <yuli45904@gmail.com>
2022-09-19 11:37:43 +0700

    Route & View

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <style>
+        h1 {
+            color : blue;
+        }
+        body {
+            background-color : purple;
+        }
+        b {
+            color : pink;
+        }
+        ul {
+            color : white;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hallo, my name is Yuliyanti Arthamevia</h1>
+    <b>Belajar route dan view Laravel</b>
+
+    <ul>
+        <li>Belajar membuat route</li>
+        <li>Belajar membuat view</li>
+        <li>Menampilkan view menggunakan route</li>
+    </ul>
+</body>
+</html>
+
+<!-- Link belajar -->
+<!-- https://www.ayongoding.com/membuat-route-dan-view-laravel/ -->
